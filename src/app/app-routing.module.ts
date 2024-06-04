f0db1a478114eae2bb0f1ab234771644caef9d54
Sean Rodriguez <sean530sly@gmail.com>
2024-06-03 23:22:45 -0500

    Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./feature-modules/home/home.module').then(m => m.HomeModule) },
   { path: 'user-management', loadChildren: () => import('./feature-modules/user-management/user-management.module').then(m => m.UserManagementModule) },
   { path: 'reporting', loadChildren: () => import('./feature-modules/reporting/reporting.module').then(m => m.ReportingModule) },
   { path: 'data-collection', loadChildren: () => import('./feature-modules/data-collection/data-collection.module').then(m => m.DataCollectionModule) }
